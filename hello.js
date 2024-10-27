edeef38192a062903991971566be7e38c0e392c8
MInhTais <minnhtai2019cb2@gmail.com>
2024-10-27 09:35:39 +0700

    Viết 1 chương trinh for.c có 1 vòng lặp hiển thị các số từ 1 đến 10

--- hello.js
+++ hello.js
@@ -4,3 +4,7 @@ function greet(name) {
 var greeting = greet("World");
 console.log(greeting);
 // thêm xuống dòng
+// thêm dòng for
+for (var index = 0; index < 11; index++) {
+    console.log(index);
+}
--- hello.ts
+++ hello.ts
@@ -5,3 +5,8 @@ function greet(name: string): string {
 const greeting = greet("World");
 console.log(greeting);
 // thêm xuống dòng
+
+// thêm dòng for
+for (let index = 0; index <11; index++) {
+    console.log(index)
+}
